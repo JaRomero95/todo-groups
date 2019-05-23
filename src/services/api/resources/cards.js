cef50dfdf937a69ad5a828d17279ae1c2f01d3fd
Juan Antonio Romero <romero.r4l@gmail.com>
2019-05-23 19:00:57 +0200

    Delete task dialog

--- src/services/api/resources/cards.js
+++ src/services/api/resources/cards.js
@@ -1,7 +1,8 @@
 import client from '../client';
 
 const cardsPath = idList => `lists/${idList}/cards`;
-const cardPath = 'cards';
+const createCardPath = 'cards';
+const cardPath = idCard => `cards/${idCard}`;
 
 async function index(idList) {
   const response = await client.get(cardsPath(idList));
@@ -10,11 +11,17 @@ async function index(idList) {
 
 async function create(idList, card) {
   const params = {idList, ...card};
-  const response = await client.post(cardPath, null, {params});
+  const response = await client.post(createCardPath, null, {params});
+  return response.data;
+}
+
+async function destroy(idCard) {
+  const response = await client.delete(cardPath(idCard));
   return response.data;
 }
 
 export default {
   index,
   create,
+  destroy,
 };
