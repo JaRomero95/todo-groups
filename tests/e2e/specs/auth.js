6ecf391fb7f51594998ef5196251d285582163b0
Juan Antonio Romero <romero.r4l@gmail.com>
2019-05-25 19:58:52 +0200

    Refactor and test auth flow
    - Added layout components and used based on route metadata
    - Extract app title H1 to AppTitle component to reuse it
    - Add test data selectors to components and add an auth-flow test

--- tests/e2e/specs/auth.js
+++ tests/e2e/specs/auth.js
@@ -3,14 +3,29 @@ describe('Login', () => {
     // TODO: get urls from api service
     cy.route('1/search*', 'fixture:boards/board.json');
     cy.route('1/boards/*/lists*', 'fixture:lists/lists.json');
+
     cy.visit('/');
   })
 
-  it('Success auth', () => {
+  it('Success auth flow', () => {
+    logoutButton().should('not.exist');
+
+    fillLoginForm();
+
+    cy.get('[data-test=group-list]');
+
+    logoutButton().click();
+
+    cy.get('[data-test=login-form]');
+  })
+
+  function fillLoginForm() {
     cy.get('[name=api-key]').type('a');
     cy.get('[name=oauth-token]').type('b');
     cy.get('[type=submit]').click();
+  }
 
-    cy.contains('Group 1');
-  })
+  function logoutButton() {
+    return cy.get('[data-test=logout]');
+  }
 })
