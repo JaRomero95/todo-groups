5601f283c752405104f8013bab6db1a1561a0e64
Juan Antonio Romero <romero.r4l@gmail.com>
2019-05-27 19:35:03 +0200

    Check token on login page querying profile

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,7 @@
 import board from './resources/board';
 import lists from './resources/lists';
 import cards from './resources/cards';
+import me from './resources/me';
 
 /**
  * Adaptation from Trello API to application Domain Specific Language
@@ -10,10 +11,12 @@ import cards from './resources/cards';
  * database*       board
  * group           list
  * task            card   
+ * profile         me
  */
 
 export default {
   board,
   groups: lists,
   tasks: cards,
+  profile: me,
 };
--- tests/e2e/support/server.js
+++ tests/e2e/support/server.js
@@ -5,4 +5,9 @@ beforeEach(() => {
     force404: true,
     baseUrl: config.TRELLO_API_URL,
   });
+
+  // TODO: get urls from api service
+  cy.route('1/search*', 'fixture:boards/board.json');
+  cy.route('1/boards/*/lists*', 'fixture:lists/lists.json');
+  cy.route('1/members/me*', 'fixture:me/me.json');
 });
